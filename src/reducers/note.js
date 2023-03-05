e5bcacce72fa258586956e2c53fafd950c7b2e1b
Marek Zelinka <mzelinka17@gmail.com>
2023-03-05 09:44:49 +0100

    set initial state

--- src/reducers/note.js
+++ src/reducers/note.js
@@ -1,8 +1,21 @@
+const INITIAL_STATE = [
+  {
+    content: "reducer defines how redux store works",
+    important: true,
+    id: 1,
+  },
+  {
+    content: "state of store can contain any data",
+    important: false,
+    id: 2,
+  },
+];
+
 function generateId() {
   return Number((Math.random() * 1_000_000).toFixed(0));
 }
 
-export function noteReducer(state = [], action) {
+export function noteReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case "ADD_NOTE": {
       const { content, important } = action.payload;
